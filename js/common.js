8ee90e962aa75371da33364a929567787924c38c
lxqswing <lxqswing@163.com>
2014-05-22 02:39:01 +0800

    change file name change banner full screen change homg_io action

--- js/common.js
+++ js/common.js
@@ -1,19 +1,20 @@
 
 $(function(){
     var bodyHeight = $(document.body).height();
-    var subNavHeight = $(window).height()-66;
+    var subNavHeight = $(window).height()-60;
     $('.nav_wrapper').css("height",bodyHeight);
     $('.sub_nav').css("height",subNavHeight);
     $(document).scroll(function(){
     	var wScroll = $(document).scrollTop();
-        var headerTop = $(window).height()-66;
+        var headerTop = $(window).height()-60;
         if(wScroll>headerTop){
         	$(".header").css({"position":"fixed",top:"0",marginTop:"0"});
             $('.banpho').css('display','none');
         }
         if(wScroll<headerTop){
-        	$(".header").css({"position":"absolute",top:"auto"});
             $('.banpho').css('display','block');
+        	$(".header").css({"position":"absolute",top:"auto"});
+            
         }
     });
     $(".sub_nav_first, .sub_nav_second, .sub_nav_third").bind('mousewheel', function(event) {
